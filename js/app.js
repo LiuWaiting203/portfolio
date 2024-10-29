ed715a75621706fd457622267f033cd85147273b
MachikadoAme <littlegreen301@gmail.com>
2024-10-29 20:07:31 +0800

    更新資訊, 新增 aplayer 音樂播放器套件

--- js/app.js
+++ js/app.js
@@ -21,6 +21,33 @@ createApp({
         backgroundColor: 'rgba(0,0,0)',
         duration: 2
       })
+
+      gsap.from("#scroll-about-me", {
+        opacity: 0,
+        y: -70,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: "#scroll-about-me",
+          start: "top 40%",
+          end: "bottom 50%",
+          toggleActions: "restart none none none",
+          // markers: true
+        }
+      })
+
+      const ap = new APlayer({
+        container: document.getElementById('aplayer'),
+        fixed: true,
+        autoplay: true,
+        loop: true,
+        audio: [{
+          name: 'TOMO - 鹿',
+          artist: 'TOMO',
+          url: '../assests/TOMO - 鹿.mp3',
+          cover: '../assests/deer.png',
+        }]
+      })
     })
   },
 }).mount("#app");
